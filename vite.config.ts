65fbd37012896932b14fdb5bdf24bf3731976f42
Vukašin Stepanović <vukasinstepanovic@gmail.com>
2022-07-31 16:18:56 +0200

    fix: Add repo name as base URL in production

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,10 @@ import react from "@vitejs/plugin-react";
 import * as path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(env => ({
+  base: env.mode === "development" ? "/" : "/qr_scanner/",
   resolve: {
     alias: [{ find: "~", replacement: path.resolve(__dirname, "src") }],
   },
   plugins: [react()],
-});
+}));
